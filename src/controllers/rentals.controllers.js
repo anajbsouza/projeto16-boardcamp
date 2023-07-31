f2da523efd5c7c68a47b7921d86c5b0ba05812c2
anajbsouza <anaju.batsouza@gmail.com>
2023-07-31 14:57:11 -0300

    feat: delete rentals

--- src/controllers/rentals.controllers.js
+++ src/controllers/rentals.controllers.js
@@ -74,8 +74,10 @@ export async function finishRental(req, res) {
 }
 
 export async function deleteRental(req, res) {
+    const { id } = req.params;
     try {
-        res.send('oi');
+        await db.query(`DELETE FROM rentals WHERE id=$1`, [id]);
+        res.sendStatus(200);
     } catch (err) {
         res.status(500).send(err.message);
     }
